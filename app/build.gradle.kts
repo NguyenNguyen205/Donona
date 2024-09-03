fe9e934097f6040268d8f60fd815b4f65775fba6
NguyenNguyen205 <s3927220@rmit.edu.vn>
2024-09-03 22:24:08 +0700

    Update profile layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,11 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("com.google.code.gson:gson:2.10.1")
 
+    // Material UI
+    implementation("com.google.android.material:material:1.3.0-alpha02")
+
+
+
 }
 
 
