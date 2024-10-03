975bc82626af5f177997b7d77e2a12e6d728e5ca
tuankiet12012003 <kietdhtse172938@fpt.edu.vn>
2024-10-03 20:24:37 +0700

    Apply Dark or Light mode

--- app/src/main/java/com/example/donona/SettingActivity.java
+++ app/src/main/java/com/example/donona/SettingActivity.java
@@ -1,6 +1,7 @@
 package com.example.donona;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
@@ -8,9 +9,11 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -29,16 +32,47 @@ import java.io.File;
 public class SettingActivity extends AppCompatActivity {
     private Button logoutButton;
 
+    //Download and play music file
     private FirebaseStorage storage;
     private StorageReference storageReference;
     private MediaPlayer mediaPlayer;
     private File localFile;
+
+    //Switch between dark and light mode
+    private Switch switchTheme;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_setting);
+
+        switchTheme = findViewById(R.id.switch_theme);
+
+        // SharedPreferences để lưu trạng thái
+        sharedPreferences = getSharedPreferences("AppSettingsPrefs", 0);
+        editor = sharedPreferences.edit();
+
+        // Kiểm tra trạng thái hiện tại của theme
+        boolean isNightMode = sharedPreferences.getBoolean("NightMode", false);
+        switchTheme.setChecked(isNightMode);
+
+        // Xử lý khi người dùng thay đổi theme
+        switchTheme.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            boolean currentMode = sharedPreferences.getBoolean("NightMode", false);
+            if (isChecked != currentMode) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor.putBoolean("NightMode", true);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor.putBoolean("NightMode", false);
+                }
+                editor.apply();  // Lưu trạng thái vào SharedPreferences
+            }
+        });
 
         // Khởi tạo FirebaseStorage và StorageReference
         storage = FirebaseStorage.getInstance();
