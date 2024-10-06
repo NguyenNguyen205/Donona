7e373ea290a84ace07c2f741b307491c7ea46979
ThanhHop-art <thanhhop489@gmail.com>
2024-10-06 16:22:01 +0700

    fix UI, profile

--- app/src/main/java/com/example/donona/SignupActivity.java
+++ app/src/main/java/com/example/donona/SignupActivity.java
@@ -2,9 +2,11 @@ package com.example.donona;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -45,6 +47,9 @@ public class SignupActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
+
+        TextView notice = (TextView) findViewById(R.id.notice);
+        notice.setText(Html.fromHtml("To continue, you can review Company’s <b>privacy policy</b> and <b>terms of service</b>."));
     }
 
     public void onSignUp(View view) {
